6f5796f1451d59634ea854755c75ce254b9d24c1
G384082023 <238408@st.takushoku-u.ac.jp>
2024-07-05 15:31:08 +0900

    数当てゲーム終わり!!!

--- kazuate.js
+++ kazuate.js
@@ -7,73 +7,70 @@ let yoso;
 let b1 = document.querySelector('button#kaito');
 b1.addEventListener('click', hantei);
 function hantei() {
+    let i = document.querySelector('input[name="number"]');
+    let number = i.value;       // ユーザが記入した文字列
+    yoso= number;
+    //何回(回数)の入力
+    let span1 = document.querySelector('span#kaisu');
+    //予想数の入力
+    let span2 = document.querySelector('span#answer');
+    
+
   if(kaisu >=0 && kaisu <=2){
-      if(kaisu == 0){
+    if(kaisu == 0){
       kaisu =1;
       //3行消し
       let w = document.querySelector('p#result > span');
       w.remove();
+  
+      span1.textContent = kaisu;
+      span2.textContent = number;
     }
-    let i = document.querySelector('input[name="number"]');
-    let number = i.value;       // ユーザが記入した文字列
-    yoso= number;
-    console.log(kaisu+'回目の予想:' +yoso);
-    
 
-  //何回(回数)の入力
-  let span1 = document.querySelector('span#kaisu');
+  console.log(kaisu+'回目の予想:' +yoso);
+  let result = document.querySelector('p#result');    
   span1.textContent = kaisu;
-  //予想数の入力
-  let span2 = document.querySelector('span#answer');
   span2.textContent = number;
 
-  if(yoso == kotae){
-    let result = document.querySelector('p#result');         
-    result.textContent = '正解です．おめでとう!';
-    console.log('正解です．おめでとう!');
-    kaisu = 5;
+    if(yoso == kotae){        
+      result.textContent = '正解です．おめでとう!';
+      console.log('正解です．おめでとう!');
+      kaisu = 5;
 
-  }else if(yoso > kotae){
-    let result = document.querySelector('p#result');      
-    result.textContent = 'まちがい．答えはもっと小さいですよ';
-    console.log('まちがい．答えはもっと小さいですよ');
-    kaisu ++;
+    }else if(yoso > kotae){
+      result.textContent = 'まちがい．答えはもっと小さいですよ';
+      console.log('まちがい．答えはもっと小さいですよ');
+      kaisu ++;
 
-  }else if(yoso < kotae){
-    let result = document.querySelector('p#result');          
-    result.textContent = 'まちがい．答えはもっと大きいですよ';
-    console.log('まちがい．答えはもっと大きいですよ');
-    
-    kaisu ++;
-  }
+    }else if(yoso < kotae){        
+      result.textContent = 'まちがい．答えはもっと大きいですよ';
+      console.log('まちがい．答えはもっと大きいですよ');
+      kaisu ++;
+    }
 
-  }else if(kaisu == 3){
-    let i = document.querySelector('input[name="number"]');
-    let number = i.value;       // ユーザが記入した文字列
-    yoso= number;
-    let span1 = document.querySelector('span#kaisu');
-    span1.textContent = kaisu;
-    let span2 = document.querySelector('span#answer');
-    span2.textContent = number;
-    console.log(kaisu+'回目の予想:' +yoso);
-    console.log('まちがい．残念でした答えは '+ kotae +'です．');
-    let result = document.querySelector('p#result');          
-    result.textContent = 'まちがい．残念でした答えは '+ kotae +'です．';
-    kaisu ++;
   }else if(kaisu >= 3){
-    let i = document.querySelector('input[name="number"]');
-    let number = i.value;       // ユーザが記入した文字列
-    yoso= number;
-    let span1 = document.querySelector('span#kaisu');
+    let result = document.querySelector('p#result');    
     span1.textContent = kaisu;
-    let span2 = document.querySelector('span#answer');
     span2.textContent = number;
-    
-    console.log(kaisu+'回目の予想:' +yoso);
-    console.log('答えは'+kotae+'でした．すでにゲームは終わっています');
-    let result = document.querySelector('p#result');          
-    result.textContent = '答えは'+kotae+'でした．すでにゲームは終わっています';
-    kaisu++;
+    if(kaisu == 3){
+      console.log(kaisu+'回目の予想:' +yoso);
+      if(yoso != kotae){   
+      console.log('まちがい．残念でした答えは '+ kotae +'です．');
+      let result = document.querySelector('p#result');          
+      result.textContent = 'まちがい．残念でした答えは '+ kotae +'です．';
+    }else {
+      console.log('正解です．おめでとう!');
+      let result = document.querySelector('p#result');          
+      result.textContent = '正解です．おめでとう!';
+    }
+    kaisu ++;
+    }else {
+      console.log(kaisu+'回目の予想:' +yoso);
+      console.log('答えは'+kotae+'でした．すでにゲームは終わっています');
+      let result = document.querySelector('p#result');          
+      result.textContent = '答えは'+kotae+'でした．すでにゲームは終わっています';
+      kaisu++;
+    }
   }
 }
 
