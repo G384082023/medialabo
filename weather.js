d5956f636d770d050b63601ba6cc48ff5f9ef41e
G384082023 <238408@st.takushoku-u.ac.jp>
2024-07-19 17:39:10 +0900

    project perfect! 点数100点お願い

--- weather.js
+++ weather.js
@@ -1,52 +1,3 @@
-/*let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-*/
-
-
 ////////// 課題3-2 ここからプログラムを書こう
 
 //print(data);
@@ -69,40 +20,10 @@ function print(data){
   
 }
 
-
-/*console.log("都市名 : " + data.name);
-console.log("緯度 : " + data.coord.lon);
-console.log("経度 : " + data.coord.lat);
-
-console.log("天気 : " + data.weather[0].description);
-
-console.log("最低気温 : " + data.main.temp_min);
-console.log("最高気温 : " + data.main.temp_max);
-
-console.log("湿度 : " + data.main.humidity);
-console.log("風速 : " + data.wind.speed);
-console.log("風向 : " + data.wind.deg);
-
-let b = document.querySelector('button#btn');
-b.addEventListener('click', showSelectResult);
-
-function showSelectResult() {
-    let s = document.querySelector('select#select');
-    let idx = s.selectedIndex;  // idx 番目の option が選択された
-
-    let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
-    let o = os.item(idx);       // os の idx 番目の要素
-
-    console.log('選択された ' + idx + ' 番目の option の情報:');
-    console.log('  value=' + o.getAttribute('value'));  // id 属性を表示
-    console.log('  textContent='+o.textContent);
-}*/
-
 let b = document.querySelector('#sendRequest');
 b.addEventListener('click', sendRequest);
 
 var body = document.querySelector('body');
-
 function changeBackground(backgroundUrl) {
   body.style.backgroundImage = 'url(' + backgroundUrl + ')';
 }
@@ -169,10 +90,10 @@ function showResult(resp) {
     
 
     // data をコンソールに出力
-    console.log(data);
+    //console.log(data);
 
     // data.x を出力
-    console.log(data.x);
+    //console.log(data.x);
 }
 
 // 通信エラーが発生した時の処理
@@ -246,17 +167,80 @@ ul.insertAdjacentElement('beforeend',li9);
 div3.insertAdjacentElement('beforeend', ul); 
 */
 
+/*console.log("都市名 : " + data.name);
+console.log("緯度 : " + data.coord.lon);
+console.log("経度 : " + data.coord.lat);
+
+console.log("天気 : " + data.weather[0].description);
+
+console.log("最低気温 : " + data.main.temp_min);
+console.log("最高気温 : " + data.main.temp_max);
+
+console.log("湿度 : " + data.main.humidity);
+console.log("風速 : " + data.wind.speed);
+console.log("風向 : " + data.wind.deg);
+
+let b = document.querySelector('button#btn');
+b.addEventListener('click', showSelectResult);
+
+function showSelectResult() {
+    let s = document.querySelector('select#select');
+    let idx = s.selectedIndex;  // idx 番目の option が選択された
+
+    let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
+    let o = os.item(idx);       // os の idx 番目の要素
+
+    console.log('選択された ' + idx + ' 番目の option の情報:');
+    console.log('  value=' + o.getAttribute('value'));  // id 属性を表示
+    console.log('  textContent='+o.textContent);
+}*/
 
 
-
-
-   
-
-
-   
-
-
-
-
-
+/*let data = {
+  "coord": {
+    "lon": 116.3972,
+    "lat": 39.9075
+  },
+  "weather": [
+    {
+      "id": 803,
+      "main": "Clouds",
+      "description": "曇りがち",
+      "icon": "04d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 9.94,
+    "feels_like": 8.65,
+    "temp_min": 9.94,
+    "temp_max": 9.94,
+    "pressure": 1022,
+    "humidity": 14,
+    "sea_level": 1022,
+    "grnd_level": 1016
+  },
+  "visibility": 10000,
+  "wind": {
+    "speed": 2.65,
+    "deg": 197,
+    "gust": 4.84
+  },
+  "clouds": {
+    "all": 53
+  },
+  "dt": 1646542386,
+  "sys": {
+    "type": 1,
+    "id": 9609,
+    "country": "CN",
+    "sunrise": 1646520066,
+    "sunset": 1646561447
+  },
+  "timezone": 28800,
+  "id": 1816670,
+  "name": "北京市",
+  "cod": 200
+};
+*/
 
